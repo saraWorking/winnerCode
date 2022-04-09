fa6262f5aff472b5d8df41af36591e2215d54a02
IMOE001 <IMOE001@MOE-0624185867>
2022-04-10 00:00:54 +0300

    add server

---
+++ test/Controllers/DataController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using test.services;
+
+namespace test.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DataController : ControllerBase
+    {
+
+        private readonly ILogger<DataController> _logger;
+        private IDataService dataService;
+
+        public DataController(ILogger<DataController> logger, IDataService iDataService)
+        {
+            _logger = logger;
+            dataService = iDataService;
+        }
+
+   
+
+        [HttpGet]
+        [Route("[action]/{from}/{itemsPerPage}")]
+        public async Task<ActionResult<List<Image>>> Page(int from, int itemsPerPage)
+        {
+            return await dataService.GetDataAsync(from, itemsPerPage);
+
+
+        }
+    }
+}
+
+
+
---
+++ test/services/DataService.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace test.services
+{
+    public interface IDataService
+    {
+        Task<List<Image>> GetDataAsync(int from, int itemsPerPage);
+    }
+
+    public class DataService : IDataService
+    {
+
+
+
+        public async Task<List<Image>> GetDataAsync(int from, int itemsPerPage)
+        {
+            var image = new Image();
+            List<Image> root;
+            var url = "https://jsonplaceholder.typicode.com/photos"; 
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(url))
+                {
+                    using (var content = response.Content)
+                    {
+                       var result = await content.ReadAsStringAsync();
+                         root = JsonConvert.DeserializeObject<List<Image>>(result);
+                       root= root.GetRange(from, itemsPerPage);
+                    }
+                }
+                return root;
+            }
+
+        }
+    }
+
+    public class Image{
+
+
+        public int albumId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string url { get; set; }
+        public string thumbnailUrl { get; set; }
+}
+
+}
